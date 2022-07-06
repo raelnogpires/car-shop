12212681818afc2d8c0c58462d7976cd0ecf3c04
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-07-06 17:49:40 -0300

    feat: create CarModel

---
+++ src/models/CarModel.ts
@@ -0,0 +1,22 @@
+import { Schema, model as createModel, Document } from 'mongoose';
+
+import { Car } from '../interfaces/CarInterface';
+
+import MongooseModel from './MongooseModel';
+
+interface CarDocument extends Car, Document {}
+
+const carSchema = new Schema<CarDocument>({
+  model: String,
+  year: Number,
+  color: String,
+  buyValue: Number,
+  doorsQty: Number,
+  seatsQty: Number,
+});
+
+export default class CarModel extends MongooseModel<CarDocument> {
+  constructor(model = createModel('cars', carSchema)) {
+    super(model);
+  }
+}
