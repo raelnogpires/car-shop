ae8696b72afec6c2072a946938ea08dda359bbd0
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-07-06 16:08:27 -0300

    fix: remove arrow function

--- interfaces/ModelInterface.ts
+++ interfaces/ModelInterface.ts
@@ -1,7 +1,7 @@
 export interface Model<T> {
-  create: (data: T) => Promise<T>;
-  read: () => Promise<T[]>;
-  readOne: (id: string) => Promise<T>;
-  update: (id: string, data: T) => Promise<T>;
-  delete: (id: string) => Promise<T>;
+  create(data: T): Promise<T>,
+  read(): Promise<T[]>;
+  readOne(id: string): Promise<T | null>;
+  update(id: string, data: T): Promise<T | null>;
+  delete(id: string): Promise<T | null>;
 }
