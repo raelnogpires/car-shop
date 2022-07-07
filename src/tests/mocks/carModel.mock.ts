95a0c5412cc93fdff986205373c6312d2a7064ae
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-07-07 19:42:08 -0300

    tests: create unit test for CarService.create method

--- src/tests/mocks/carModel.mock.ts
+++ src/tests/mocks/carModel.mock.ts
@@ -2,14 +2,14 @@ import { Car } from '../../interfaces/CarInterface';
 import { Model } from '../../interfaces/ModelInterface';
 import { carMock } from './car.mock';
 
-class CarModelMock implements Model<Car> {
+export default class CarModelMock implements Model<Car> {
   create = async (data: Car): Promise<Car> => data;
 
   read = async (): Promise<Car[]> => [carMock];
 
-  readOne = async (id: string): Promise<Car | null> => carMock;
+  readOne = async (_id: string): Promise<Car | null> => carMock;
 
-  update = async (id: string, data: Car): Promise<Car | null> => data;
+  update = async (_id: string, data: Car): Promise<Car | null> => data;
 
-  delete = async (id: string): Promise<Car | null> => carMock;
+  delete = async (_id: string): Promise<Car | null> => carMock;
 }
--- src/tests/unit/services/CarService.test.ts
+++ src/tests/unit/services/CarService.test.ts
@@ -1,3 +1,16 @@
-import { expect } from 'chai';
-import mongoose from 'mongoose';
+import { expect } from 'chai'
+import CarService from '../../../services/CarService';
+import { carMock } from '../../mocks/car.mock';
+import CarModelMock from '../../mocks/carModel.mock';
 
+describe('02 - Tests CarService', () => {
+  const modelMock = new CarModelMock();
+
+  describe('CarService.create', () => {});
+  it('Success', async () => {
+    // @ts-ignore
+    const service = new CarService(modelMock);
+    const created = await service.create(carMock);
+    expect(created).to.deep.equal(carMock);
+  });
+});
